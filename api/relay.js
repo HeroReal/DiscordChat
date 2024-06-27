80d3b188d6a8f35a060b2513c09eb23f14f5a65a
HeroReal <ardilkasacibackup@gmail.com>
2024-06-27 21:56:01 +0200

    relay.js

--- api/relay.js
+++ api/relay.js
@@ -6,13 +6,15 @@ module.exports = async (req, res) => {
     const DISCORD_WEBHOOK_URL = 'https://webhook.lewisakura.moe/api/webhooks/1213511752403263570/885voICZXScAfoqapr77-Qxg9z-z-eTUGmH-0jJhq2d5aJm9l4PTChjIHupALFM3Xahv';
 
     try {
-        await axios.post(DISCORD_WEBHOOK_URL, {
+        const response = await axios.post(DISCORD_WEBHOOK_URL, {
             username: playerName,
             content: message,
         });
+
+        console.log('Message sent to Discord:', response.data);
         res.status(200).send('Message sent to Discord!');
     } catch (error) {
-        console.error('Error sending message to Discord:', error);
+        console.error('Error sending message to Discord:', error.message);
         res.status(500).send('Failed to send message to Discord.');
     }
 };
